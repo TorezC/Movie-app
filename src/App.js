3f4cf17cc44f1d7b6150f509e3e16538d1f25886
TorezC <victorytorez@gmail.com>
2022-07-21 15:28:09 +0100

    created the movie-page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Movies from './components/Movies/Movies';
+import Search from './components/Search/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <div className="slide-img">
+        <p>Watch something incredible.</p>
+      </div>
+      <div className='container'>
+        <Search/>
+        <Movies/>
+      </div>
     </div>
   );
 }
